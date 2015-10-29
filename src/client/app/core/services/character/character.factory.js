c1d0f136713feab962ed2b037e70151b564a5936
keithhopkins <knhopkins412@gmail.com>
2015-10-29 13:59:13 -0600

    adds characterservice

--- src/client/app/core/services/character/character.factory.js
+++ src/client/app/core/services/character/character.factory.js
@@ -5,21 +5,45 @@
     .factory('characterservice', characterFactory);
 
   function characterFactory(){
-    var factory = {
-      setAttributes: setAttributes
-    };
-
-    return factory;
     var character = {};
-
-    function setAttributes(attributes){
-      character.attributes = attributes;
-      console.log(character.attributes);
-    }
-
-    function setSkills(skills){
-      character.skills = skills;
-    }
+    return {
+      setAttributes: function(attributes){
+        character.attributes = attributes;
+      },
+      setSkills: function(skills){
+        character.skills = skills;
+      },
+      setKnowledge: function(knowledge){
+        character.knowledge = knowledge;
+      },
+      setPersonalData: function(data){
+        character.personalData = personalData;
+      },
+      setQualities: function(qualities){
+        character.qualities = qualities;
+      },
+      setItems: function(items){
+        character.items = items;
+      },
+      getAttributes: function(){
+        return character.attributes;
+      },
+      getSkills: function(){
+        return character.skills;
+      },
+      getKnowledge: function(){
+        return character.knowledge;
+      },
+      getPersonalData: function(){
+        return character.personalData;
+      },
+      getQualities: function(){
+        return character.qualities;
+      },
+      getItems: function(){
+        return character.items;
+      }
+    };
 
   }
 
